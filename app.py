dc2ea8c33a86d18e5fa0f4f692495a015ea31f21
anas <anasaljagbeer@gmail.com>
2024-01-08 16:17:03 +0300

    ss

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from pymongo import MongoClient
 from dotenv import load_dotenv
-import certifi
+# import certifi
 
 load_dotenv()
 
@@ -16,7 +16,8 @@ def create_app():
     # client = MongoClient(os.getenv("MONGODB_URI"))
     app.config["MONGODB_URI"] = os.environ.get("MONGODB_URI")
     app.config["MONGODB_DB"] = os.environ.get("MONGODB_DB")
-    app.db = MongoClient(app.config["MONGODB_URI"], tlsCAFile=certifi.where())[
+    # , tlsCAFile=certifi.where()
+    app.db = MongoClient(app.config["MONGODB_URI"])[
         app.config.get("MONGODB_DB")
     ]
     app.config["SECRET_KEY"] = SECRET_KEY
